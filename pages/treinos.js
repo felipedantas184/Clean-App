ee719ba4ce81daaf95b0dad318667a3fd47530d7
Felipe <felipedantas184@gmail.com>
2022-08-06 12:16:25 -0300

    treinos

--- pages/treinos.js
+++ pages/treinos.js
@@ -6,19 +6,19 @@ const Treinos = () => {
   return ( 
     <>
     <Head>
-      <title>Vantagens Carcará | Medicina UFPI</title>
-      <meta name="description" content="Vantagens para os associados da A.A.A. Carcará" />
+      <title>Treinos Carcará | Medicina UFPI</title>
+      <meta name="description" content="Treinos para os associados da A.A.A. Carcará" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       
-      <meta property="og:title" content="Vantagens Carcará | Medicina UFPI"/>
+      <meta property="og:title" content="Treinos Carcará | Medicina UFPI"/>
       <meta property="og:type" content="school"/>
-      <meta property="og:description" content="Vantagens para os associados da A.A.A. Carcará"/>
+      <meta property="og:description" content="Treinos para os associados da A.A.A. Carcará"/>
       <meta property="og:image" content="/apple-touch-icon.png"/>
       <meta property="og:site_name" content="A.A.A. Carcará"/>
 
-      <meta property="twitter:title" content="Vantagens Carcará | Medicina UFPI"/>
-      <meta property="twitter:url" content="https://carcara.vercel.app/vantagens"/>
-      <meta property="twitter:description" content="Vantagens para os associados da A.A.A. Carcará"/>
+      <meta property="twitter:title" content="Treinos Carcará | Medicina UFPI"/>
+      <meta property="twitter:url" content="https://carcara.vercel.app/treinos"/>
+      <meta property="twitter:description" content="Treinos para os associados da A.A.A. Carcará"/>
       <meta property="twitter:image" content="/apple-touch-icon.png"/>
     </Head>
     <Layout>
