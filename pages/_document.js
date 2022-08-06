43380f71019b0eea2389a76514022344b7be07d3
Felipe <felipedantas184@gmail.com>
2022-08-06 12:12:24 -0300

    document.js

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,12 @@ import { ServerStyleSheet } from 'styled-components'
             <link href="https://fonts.googleapis.com/css2?family=Graduate&display=swap" rel="stylesheet" />
             <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+          
             <meta name="theme-color" content="#051937" />
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           </Head>
--- pages/carteira-digital/[slug].js
+++ pages/carteira-digital/[slug].js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import MemberCard from '../../components/MemberCard';
 import { MembersData } from '../../components/MemberCard/MembersData';
 import Layout from '../../components/Layout/Layout';
@@ -26,8 +27,26 @@ export const getStaticProps = async (context) => {
 
 export default function RecipeDetails({ member }) {
   return (
+    <>
+    <Head>
+      <title>Carteira Digital | A.A.A. Carcará</title>
+      <meta name="description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      
+      <meta property="og:title" content="Carteira Digital | A.A.A. Carcará | Medicina UFPI"/>
+      <meta property="og:type" content="school"/>
+      <meta property="og:description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013"/>
+      <meta property="og:image" content="/apple-touch-icon.png"/>
+      <meta property="og:site_name" content="A.A.A. Carcará"/>
+
+      <meta property="twitter:title" content="Carteira Digital | A.A.A. Carcará | Medicina UFPI"/>
+      <meta property="twitter:url" content="https://carcara.vercel.app/"/>
+      <meta property="twitter:description" content="🦅 Associação Atlética Acadêmica da Medicina UFPI | 🏆 A primeira e maior do PI, desde 2013"/>
+      <meta property="twitter:image" content="/apple-touch-icon.png"/>
+    </Head>
     <Layout>
       <MemberCard member={member} />
     </Layout>
+    </>
   )
 }
